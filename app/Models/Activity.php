9ec0c226925eadce6179f175d651b126f406b319
Mati <mateusz4k@outlook.com>
2026-04-03 19:04:07 +0000

    WIP: Ts integration, seeders implementation and build workflow

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'type', 'distance', 'duration', 'date', 'notes', 'weather_json',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
